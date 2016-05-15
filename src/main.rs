70b2db175325aa0269c70a7a64dcfcdab4cd79ab
Jadon Fowler <jadonflower@gmail.com>
2016-05-15 15:06:05 -0700

    Send music file to client and play it
    The Rust server now sends the files needed to play the requested track.
    Playlists will need to be implemented a different way.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ use liquid::{Renderable, Context, Value};
 
 fn handler(req: Request, mut res: Response) {
     if let RequestUri::AbsolutePath(url) = req.uri {
+        println!("GET {}", &url);
         let song = format!("music{}.mp3", &url);
-        let md = metadata(song);
+        let md = metadata(song.clone());
         match md {
             Ok(m) => {
                 if m.is_file() { // Single Song
+                    println!("{} is a song", song.clone());
                     // Read file contents into string
                     let mut file = File::open("static/song.html").unwrap();
                     let mut html = String::new();
@@ -33,14 +35,35 @@ fn handler(req: Request, mut res: Response) {
                     let mut res = res.start().unwrap();
                     res.write_all(body.as_bytes()).unwrap();
                 }
-                else if m.is_dir() { // Playlist
-                    res.send(b"Playlists unimplemented").unwrap();
-                }
-                else {
-                   res.send(b"Song not found").unwrap();
-                }
             },
-            Err(_) => res.send(b"Song not found").unwrap()
+            Err(_) => {
+                let file_name = url[1..].to_string();
+                println!("{} isn't a song", &file_name);
+                let url_md = metadata(&file_name);
+                match url_md {
+                    Ok(m) => {
+                        print!("Metadata exists for {}. ", &file_name);
+                        if m.is_file() {
+                            // Put file bytes into buffer
+                            let mut file = File::open(&file_name).unwrap();
+                            let mut buffer = Vec::new();
+                            file.read_to_end(&mut buffer).unwrap();
+
+                            // Send file
+                            println!("{} is a file of {} bytes", &file_name, buffer.len());
+                            res.headers_mut().set(ContentLength(buffer.len() as u64));
+                            let mut res = res.start().unwrap();
+                            for byte in &buffer {
+                                res.write(&[*byte]).unwrap();
+                            }
+                            res.flush().unwrap();
+                        } else if m.is_dir() {
+                            println!("{} is a directory!", &file_name);
+                        }
+                    },
+                    Err(_) => {}
+                }
+            }
         }
 
     }
