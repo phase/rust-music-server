214ade270bb9eba68bdec9d4b35fbf14c67911f0
Jadon Fowler <jadonflower@gmail.com>
2016-05-15 16:17:55 -0700

    Playlist support
    The songs aren't being sent, something to do with HTTP encoding.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ extern crate hyper;
 extern crate liquid;
 
 use std::io::prelude::*;
-use std::fs::{File, metadata};
+use std::fs::{File, metadata, read_dir};
 use hyper::header::{ContentLength, ContentType};
 use hyper::server::{Server, Response, Request};
 use hyper::uri::RequestUri;
 use liquid::{Renderable, Context, Value};
-
 
 fn handler(req: Request, mut res: Response) {
     if let RequestUri::AbsolutePath(url) = req.uri {
@@ -37,31 +36,63 @@ fn handler(req: Request, mut res: Response) {
                 }
             },
             Err(_) => {
-                let file_name = url[1..].to_string();
-                println!("{} isn't a song", &file_name);
-                let url_md = metadata(&file_name);
-                match url_md {
-                    Ok(m) => {
-                        print!("Metadata exists for {}. ", &file_name);
-                        if m.is_file() {
-                            // Put file bytes into buffer
-                            let mut file = File::open(&file_name).unwrap();
-                            let mut buffer = Vec::new();
-                            file.read_to_end(&mut buffer).unwrap();
-
-                            // Send file
-                            println!("{} is a file of {} bytes", &file_name, buffer.len());
-                            res.headers_mut().set(ContentLength(buffer.len() as u64));
-                            let mut res = res.start().unwrap();
-                            for byte in &buffer {
-                                res.write(&[*byte]).unwrap();
-                            }
-                            res.flush().unwrap();
-                        } else if m.is_dir() {
-                            println!("{} is a directory!", &file_name);
+                let playlist = format!("music{}", &url);
+                let playlist_md = metadata(playlist.clone());
+                if let Ok(playlist_md) = playlist_md {
+                    if playlist_md.is_dir() {
+                        let paths = read_dir(&playlist).unwrap();
+                        let mut songs = Vec::new();
+                        for path in paths {
+                            let s = path.unwrap();
+                            let s = s.path();
+                            let s = s.into_os_string();
+                            let s = s.into_string();
+                            let s = s.unwrap();
+                            songs.push(Value::Str(s));
                         }
-                    },
-                    Err(_) => {}
+                        println!("Requested Playlist: {}", playlist);
+
+                        let mut file = File::open("static/playlist.html").unwrap();
+                        let mut html = String::new();
+                        file.read_to_string(&mut html).unwrap();
+                        let template = liquid::parse(&html, Default::default()).unwrap();
+                        let mut context = Context::new();
+                        context.set_val("playlist", Value::Str(playlist));
+                        context.set_val("songs", Value::Array(songs));
+
+                        let body = template.render(&mut context).unwrap().unwrap();
+                        res.headers_mut().set(ContentLength(body.len() as u64));
+                        res.headers_mut().set(ContentType::html());
+                        let mut res = res.start().unwrap();
+                        res.write_all(body.as_bytes()).unwrap();
+                    }
+                } else {
+                    let file_name = url[1..].to_string();
+                    println!("{} isn't a song", &file_name);
+                    let url_md = metadata(&file_name);
+                    match url_md {
+                        Ok(m) => {
+                            print!("Metadata exists for {}. ", &file_name);
+                            if m.is_file() {
+                                // Read file bytes into buffer
+                                let mut file = File::open(&file_name).unwrap();
+                                let mut buffer = Vec::new();
+                                file.read_to_end(&mut buffer).unwrap();
+
+                                // Send file
+                                println!("{} is a file of {} bytes", &file_name, buffer.len());
+                                res.headers_mut().set(ContentLength(buffer.len() as u64));
+                                let mut res = res.start().unwrap();
+                                for byte in &buffer {
+                                    res.write(&[*byte]).unwrap();
+                                }
+                                res.flush().unwrap();
+                            } else if m.is_dir() {
+                                println!("{} is a directory!", &file_name);
+                            }
+                        },
+                        Err(_) => {}
+                    }
                 }
             }
         }
